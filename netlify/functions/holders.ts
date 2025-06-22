57b594ba8bf6efadb6cc060cd4abffec461f5e6d
User <user@yuzanoMacBook-Air.local>
2025-06-22 23:06:52 +0900

    fix: use correct env var for Alchemy API key

--- netlify/functions/holders.ts
+++ netlify/functions/holders.ts
@@ -1,6 +1,6 @@
 console.log("ALCHEMY_API_KEY:", process.env.ALCHEMY_API_KEY);import type { Handler } from '@netlify/functions';
 
-const ALCHEMY_API_KEY = process.env.VITE_ALCHEMY_API_KEY;
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const COLLECTION_ADDRESS = "0x1d2a6e6a5e3e7e3e7e5e4e6e7e3e6e2e3e6e7e7e8";
 const BASE_URL = `https://eth-mainnet.g.alchemy.com/nft/v2/${ALCHEMY_API_KEY}`;
 
