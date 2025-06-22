1bab8919ac02ee385d2fcabc7ba44bdb907fa4a4
User <user@yuzanoMacBook-Air.local>
2025-06-22 23:32:31 +0900

    fix: use correct contract address for NFT collection

--- netlify/functions/holders.ts
+++ netlify/functions/holders.ts
@@ -1,7 +1,7 @@
 console.log("ALCHEMY_API_KEY:", process.env.ALCHEMY_API_KEY);import type { Handler } from '@netlify/functions';
 
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
-const COLLECTION_ADDRESS = "0x1d2a6e6a5e3e7e3e7e5e4e6e7e3e6e2e3e6e7e7e8";
+const COLLECTION_ADDRESS = "0x0e6a70cb485Ed3735Fa2136E0D4aDC4BF5456F93";
 const BASE_URL = `https://eth-mainnet.g.alchemy.com/nft/v2/${ALCHEMY_API_KEY}`;
 
 export const handler: Handler = async (event, context) => {
